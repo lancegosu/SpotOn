31556c2a9cbb7fea1a91753ef2a75e74721c5540
Lance <106897295+lancerai@users.noreply.github.com>
2023-12-16 22:58:31 -0800

    Update urls.py

--- places/urls.py
+++ places/urls.py
@@ -1,10 +1,7 @@
-# places/urls.py
-
 from django.urls import path
 from .views import search_places, ask_question
 
 urlpatterns = [
-    # path('search/', search_places, name='search_places'),
     path('', search_places, name='search_places'),
     path('ask-question/', ask_question, name='ask_question'),
 ]
