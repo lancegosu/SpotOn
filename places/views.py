99a0c3c314bdc6e1ce38921da720f5790fa2a224
Lance <106897295+lancerai@users.noreply.github.com>
2023-12-16 22:58:50 -0800

    Update views.py

--- places/views.py
+++ places/views.py
@@ -5,23 +5,24 @@ from django.http import JsonResponse
 from django.conf import settings
 
 # Create your views here.
+
+# View for searching places using the Google Places API
 def search_places(request):
     gmaps_api_key = settings.GMAPS_API_KEY
     results = []
 
     if request.method == 'POST':
+        # Process the search form if it's a POST request
         form = PlaceSearchForm(request.POST)
         if form.is_valid():
             query = form.cleaned_data['query']
 
             # Make a request to the Google Places API
-            # gmaps_api_key = settings.GMAPS_API_KEY
             base_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
             params = {
                 'query': query,
                 'key': gmaps_api_key,
             }
-
             response = requests.get(base_url, params=params)
             data = response.json()
 
@@ -36,7 +37,6 @@ def search_places(request):
                         'place_id': place['place_id'],
                         'details': {
                             'business_status': place.get('business_status', 'N/A'),
-                            # 'opening_hours': place.get('opening_hours', 'N/A'),#{}).get('weekday_text', 'N/A'),
                             'price_level': place.get('price_level', 'N/A'),
                             'rating': place.get('rating', 'N/A'),
                             'types': place.get('types', []),
@@ -52,6 +52,7 @@ def search_places(request):
 
     return render(request, 'search.html', {'form': form, 'results': results, 'api_key': gmaps_api_key})
 
+# Function to get reviews from the Google Places API (Place Details)
 def get_reviews_from_api(place_id):
     # Make a request to the Google Places API (Place Details) to fetch reviews
     gmaps_api_key = settings.GMAPS_API_KEY
@@ -61,35 +62,23 @@ def get_reviews_from_api(place_id):
         'key': gmaps_api_key,
         'fields': 'reviews',
     }
-
     response = requests.get(details_url, params=details_params)
     details_data = response.json()
-    # print("getreviewsfromapitest" + place_id)
-    # print(details_data)
 
-    # Extract and return reviews
-    return details_data.get('result', {}).get('reviews', [])
+    return details_data.get('result', {}).get('reviews', []) # Extract and return reviews
 
+# View for handling questions and generating answers using ChatGPT
 def ask_question(request):
     if request.method == 'POST':
+        # Get information from the POST request
         place_name = request.POST.get('place_name')
         place_address = request.POST.get('place_address')
         question = request.POST.get('question')
         place_id = request.POST.get('place_id')
-        # print(place_name)
-        print(place_address)
-        print(question)
-        # print(place_id)
-        # Fetch reviews for the selected place
-        reviews = get_reviews_from_api(place_id)
-
-        # Join all the review texts into one string
-        reviews_text = "".join([review['text'] for review in reviews])
-        print(len(reviews))
-        print(reviews_text)
-
-        # Construct the prompt for ChatGPT
-        prompt = f"Use the reviews: {reviews_text}, from {place_name} at {place_address}, to answer the given query: {question}."
+        
+        reviews = get_reviews_from_api(place_id) # Fetch reviews for the selected place
+        reviews_text = "".join([review['text'] for review in reviews]) # Join all the review texts into one string
+        prompt = f"Use the reviews: {reviews_text}, from {place_name} at {place_address}, to answer the given query: {question}." # Construct the prompt for ChatGPT
 
         # Make a request to the ChatGPT API
         openai_api_key = settings.OPENAI_API_KEY
@@ -106,12 +95,11 @@ def ask_question(request):
             ],
         }
 
+        # Send the request and extract the answer from ChatGPT's response
         response = requests.post(openai_api_url, json=data, headers=headers)
         data = response.json()
-
-        # Extract the answer from ChatGPT's response
         answer = data['choices'][0]['message']['content']
-
         return JsonResponse({'answer': answer})
+        
     else:
         return render(request, '404.html')  # Handle GET requests appropriately
